cde6ceba0d05596ed2008cddc172e05f36e35b22
npickard <npickard@platformq.com>
2017-02-22 16:22:57 -0500

    Added MyRESTController.  Confirmed working by going to http://localhost:8080 and http://localhost:8080/test

---
+++ src/main/java/com/npickard/MyRESTController.java
@@ -0,0 +1,23 @@
+package com.npickard;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by npickard on 2/20/2017.
+ */
+@RestController
+public class MyRESTController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "This is a message from MyHelloController!";
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+        return "This is a TEST message from MyHelloController!";
+    }
+
+
+}
